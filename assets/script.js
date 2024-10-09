fc4b0243a50b1e6439118753ff682aa42efe8c22
thiagofjau <tffjau@gmail.com>
2024-10-09 07:29:00 -0300

    seção depoimentos

--- assets/script.js
+++ assets/script.js
@@ -6,11 +6,39 @@ document.addEventListener('DOMContentLoaded', function () {
     burger.addEventListener('click', function () {
         nav.classList.toggle('nav-active');
         if (nav.classList.contains('nav-active')) {
-        burgerIcon.classList.remove('fa-bars');
-        burgerIcon.classList.add('fa-times');
+            burgerIcon.classList.remove('fa-bars');
+            burgerIcon.classList.add('fa-times');
         } else {
-        burgerIcon.classList.remove('fa-times');
-        burgerIcon.classList.add('fa-bars');
+            burgerIcon.classList.remove('fa-times');
+            burgerIcon.classList.add('fa-bars');
         }
     });
+});
+
+let currentSlide = 0;
+    const slides = document.querySelectorAll('.testimonial');
+    const dots = document.querySelectorAll('.dot');
+
+    function showSlide(index) {
+        const container = document.querySelector('.testimonial-container');
+        const slideWidth = slides[0].clientWidth;
+        container.style.transform = `translateX(-${index * slideWidth}px)`;
+
+        dots.forEach((dot, i) => {
+            dot.classList.toggle('active', i === index);
+        });
+    }
+
+    function nextSlide() {
+        currentSlide = (currentSlide + 1) % slides.length;
+        showSlide(currentSlide);
+    }
+
+    setInterval(nextSlide, 4000);
+
+    dots.forEach((dot, i) => {
+        dot.addEventListener('click', () => {
+            currentSlide = i;
+            showSlide(i);
+        });
     });
